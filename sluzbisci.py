0aa14da7ee4c5cbf26de3a677fe74b8c4ef5f470
mmackiewicz97 <97themati@gmail.com>
2019-02-25 20:03:25 +0100

    First evacuation

--- sluzbisci.py
+++ sluzbisci.py
@@ -5,15 +5,16 @@ import re
 from pydoc import help
 import easygui
 #path = easygui.fileopenbox()
-path = "/home/mateusz/Pulpit/3Grudzień.xlsx"
+path = "/home/mateusz/Pulpit/5Luty.xlsx"
 grafik = xlrd.open_workbook(path).sheet_by_index(0)
 
 class Rozkaz:
 
     def __init__(self):
-        self.dzien = 7
+        self.dzien = 21
         self.sluzby = []
         self.odwod = []
+        self.wolne = []
 
     def podaj_dzien(self):
         for col in range(grafik.ncols):
@@ -44,6 +45,13 @@ class Rozkaz:
                 i += 1
         self.odwod.insert(0, "Dyżur w odwodzie - " +str(len(self.odwod)))
 
+    def wolnesz(self):
+        for row in range(111):
+            if grafik.cell_value(row, self.dzien) == "":
+                if grafik.cell_value(row, 2):
+                    self.wolne.append("0 {} {} ".format(grafik.cell_value(row, 2), grafik.cell_value(row, 3)))
+        self.wolne.insert(0, "Przepustki - " +str(len(self.wolne)))
+
     def zapisz(self):
         with open("/home/mateusz/Pulpit/sluzby.txt", "w") as text_file:
             print(path, file=text_file)
@@ -53,6 +61,9 @@ class Rozkaz:
             print("\n", file=text_file)
             for dana in self.sluzby:
                 print(dana[0], "- ", dana[1], " ", dana[2], file=text_file)
+            print("\n", file=text_file)
+            for i in self.wolne:
+                print(i, file=text_file)
             text_file.close()
             # for i, dana in zip(self.odwod, self.sluzby):
             #     print(i, "\t \t", dana[0], "- ", dana[1], " ", dana[2], file=text_file)
@@ -72,55 +83,51 @@ class Student:
         self.podzial = 0
         self.miasto = 0
 
-czain = chain(range(5,36), range(43,73), range(81, 111))
-students = []
-for i in czain:
-    x = Student()
-    x.imie = grafik.cell_value(i, 3)
-    x.nazwisko = grafik.cell_value(i, 2)
-    x.row = i
-    students.append(x)
-biuro = r'B\w+'
-b = re.compile(biuro)
-podzial = r'P[^D]'
-p = re.compile(podzial)
-dyz = r'\w?D'
-d = re.compile(dyz)
-for i in students:
-    for dni in range(4,38):
-        if grafik.cell_value(i.row, dni):
-            if grafik.cell_value(i.row, dni) == 'O':
-                o = str(grafik.cell_value(4,dni))+'.12.18'
-                i.odwod.append(o)
-            elif d.match(grafik.cell_value(i.row, dni)):
-                i.dyzurka+=1
-            elif grafik.cell_value(i.row, dni) == 'M':
-                i.miasto+=1
-            elif b.match(grafik.cell_value(i.row, dni)):
-                pass
-            elif p.match(grafik.cell_value(i.row, dni)):
-                pass #dodać SK
-            else: #źle działa
-                #print(grafik.cell_value(i.row, dni), 'p')
-                i.podzial+=1
-with open("/home/mateusz/Pulpit/sluzy.txt", "w") as text_file:
-    for i in students:
-        print(i.imie, i.nazwisko, 'odwod = ', len(i.odwod), '; dyzurka = ', i.dyzurka, '; miasto = ', i.miasto, '; podzial = ', i.podzial, file=text_file)
-        print("\n", file=text_file)
-    text_file.close()
+#czain = chain(range(5,36), range(43,73), range(81, 111))
+#students = []
+#for i in czain:
+#    x = Student()
+#    x.imie = grafik.cell_value(i, 3)
+#    x.nazwisko = grafik.cell_value(i, 2)
+#    x.row = i
+#    students.append(x)
+#biuro = r'B\w+'
+#b = re.compile(biuro)
+#podzial = r'P[^D]'
+#p = re.compile(podzial)
+#dyz = r'\w?D'
+#d = re.compile(dyz)
+#for i in students:
+#    for dni in range(4,grafik.ncols):
+#        if grafik.cell_value(i.row, dni):
+#            if grafik.cell_value(i.row, dni) == 'O':
+#                o = str(grafik.cell_value(4,dni))+'.12.18'
+#                i.odwod.append(o)
+#            elif d.match(grafik.cell_value(i.row, dni)):
+#                i.dyzurka+=1
+#            elif grafik.cell_value(i.row, dni) == 'M':
+#                i.miasto+=1
+#            elif b.match(grafik.cell_value(i.row, dni)):
+#                pass
+#            elif p.match(grafik.cell_value(i.row, dni)):
+#                pass #dodać SK
+#            else: #źle działa
+#                #print(grafik.cell_value(i.row, dni), 'p')
+#                i.podzial+=1
+#with open("/home/mateusz/Pulpit/sluzy.txt", "w") as text_file:
+#    for i in students:
+#        print(i.imie, i.nazwisko, 'odwod = ', len(i.odwod), '; dyzurka = ', i.dyzurka, '; miasto = ', i.miasto, '; podzial = ', i.podzial, file=text_file)
+#        print("\n", file=text_file)
+#    text_file.close()
 
-# for col in range(4,38):
-#    print("ID:", col, "-", grafik.cell_value(4, col))
+#test.podaj_dzien()
 
+test.odwody()
 
-# test.podaj_dzien()
+test.slozby()
+test.wolnesz()
+test.zapisz()
 
-# test.odwody()
-
-# test.slozby()
-
-# test.zapisz()
-
-# test.otworz()
+test.otworz()
 
 print("Process finished successful")
