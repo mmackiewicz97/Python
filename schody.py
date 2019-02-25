7476a7fe4135750c35aaa07a17828c0379cb1c8d
mmackiewicz97 <97themati@gmail.com>
2019-02-25 20:54:03 +0100

    First and half

--- schody.py
+++ schody.py
@@ -11,107 +11,84 @@ from matplotlib import patches as mpaths# }}}
 # 10m/0,3cm na człowieka = 33osoby zmieszczą się na klatce 3 piętrowej
 # jeden w jedną komórkę
 # 1/3 z boku, 2/3 z góry, łączenie 
-floor_space = 2# {{{
+floor_space = 1# {{{
 liczba_agentow = 3
-liczba_krokow = 15
+liczba_krokow = 9
 czas_interwalu = 50
 area_of_people = 0.5
 area_of_staircase = 9# }}}
 class Agent:# {{{
 
     def __init__(self):
-        self.name = ""
+        self.name = ''
         floor = [0, 3, 6, 9, 12]
         self.position = (0.5, floor[random.randint(0, 5)])# }}}
-        self.outside = 0
 a, b, c, d, e = Agent(),  Agent(), Agent(), Agent(), Agent()# {{{
-#a.position = (0.5,0)
-#b.position = (0.5,1)
-#c.position = (0.5,2)
-#d.position = (0.5,3)
-#e.position = (0.5,6)
-#a.name="a"
-#b.name="b"
-#c.name="c"
-#d.name="d"
-#e.name="e"# }}}
+a.position = (0.5,0)
+b.position = (0.5,1)
+c.position = (0.5,2)
+d.position = (0.5,3)
+e.position = (0.5,6)
+a.name = "a"
+b.name = "b"
+c.name = "c"
+d.name = "d"
+e.name = "e" #}}}
+#zmienna zdefiniowana w init, nie da się odwołać do niej w innej metodzie ale A.zmienna istnieje
 class Pedestrians:
 
     def __init__(self):
-        y = 0 
-        self.agent = []
-        for i in 'abcdefg':
-            x = Agent()
-            x.position = (0.5, 0+y)
-            y+=1
-            x.name = i
-            self.agent.append(x)                
-
+        self.agent = [b, a, c, d,e]
         self.traj = []
-        self.floorque = {}
         #self.add_agent()
-        self.do_traj()
+        self.loop()
     def add_agent(self):
         for i in range(liczba_agentow):
             self.agent.append(Agent())
     def do_traj(self):
-        for i in range(liczba_krokow):
-            for agent in self.agent:
-                pietro = math.floor(agent.position[1]/3)
-                if agent.outside == 0:
-                    if agent.position[0] >= 1:
-                        if pietro in self.floorque:
-                            if agent not in self.floorque[pietro]:
-                                self.floorque[pietro].append(agent)
-                        else:
-                            self.floorque[pietro]=[agent]
-                    else:
-                        if pietro in self.floorque:
-                            if len(self.floorque[pietro]) < floor_space:
-                                try:
-                                    new = self.floorque[pietro+1].pop(0)
-                                    new.position = (1, pietro*3)
-                                    self.floorque[pietro].append(new)
-                                    if len(self.floorque[pietro]) < floor_space:
-                                        try:
-                                            new = self.floorque[pietro+1].pop(0)
-                                            new.position = (1, pietro*3)
-                                            self.floorque[pietro].append(agent)
-                                            agent.position = (1, agent.position[1])
-                                            self.floorque[pietro].append(new)
-                                        except:
-                                            self.floorque[pietro].append(agent)
-                                            agent.position = (1, agent.position[1])
-                                except:
-                                    self.floorque[pietro].append(agent)
-                                    agent.position = (1, agent.position[1])
-                        else:
-                            self.floorque[pietro]=[agent]
-                            agent.position = (1, agent.position[1])
-            print("krok: ", i)
-            for floor in self.floorque.keys():
-                #self.floorque[floor].sort(key=lambda x: x.position[1])
-                if len(self.floorque[floor]) < floor_space:
+        traj = []
+        x = 0
+        for agent in self.agent:
+            pietro = math.floor(agent.position[1]/3)
+            try:
+                if agent not in self.floorque[pietro]:
+                    print('nie w kolejce', agent.name)
                     try:
-                        self.floorque[floor].append(self.floorque[floor+1].pop(0))
+                        while len(self.floorque[pietro]) <= floor_space:
+                            x+=1
+                            if x%3==0:
+                                x = 0
+                                self.floorque[pietro].append(agent)
+                                break
+                            else:
+                                try:
+                                    self.floorque[pietro].append(self.floorque[pietro+1].pop(0))
+                                except KeyError:
+                                    pass
                     except:
                         pass
-                print(floor,": {}".format([agent.name for agent in self.floorque[floor]]))
-            print("\n")
-            #for i in range(len(self.floorque[key])):
-             #       self.floorque[key][i].position=(1, key+i/10)
-            if len(self.floorque[0])<1:
-                print("zakonczono w: ", i, " krokach")
-                break
-            out = self.floorque[0].pop(0)
-            out.position = (0, 0)
-            out.outside = 1
-            traj = []
-            for agent in self.agent:
-                traj.append(agent.position)
-        self.traj.append(traj)
-
+            except:
+                self.floorque[pietro]=[agent]
+        for key in self.floorque.keys():
+            self.floorque[key].sort(key=lambda x: x.position[1])
+            for i in range(len(self.floorque[key])):
+                self.floorque[key][i].position=(1, key+i/10)
+                print(self.floorque[key], key)
+        for agent in self.agent:
+            traj.append(agent.position)
+        self.floorque[0][0].position = (0, 0)
+        self.floorque[0].pop(0)
+        self.agent.remove(self.floorque[0][0])
+        return traj
+    def loop(self):
+        self.floorque = {}
+        for i in range(4):
+            self.traj.append(self.do_traj())
 A = Pedestrians()
+#print(A.x)
+#print(dir(A))
+for i in A.traj:
+    print(i)
 class Animation:# {{{
 
     def __init__(self):
