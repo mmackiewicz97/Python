b091a5674a4dc6e84b69a3455fe8b0d81ecbfa2a
Mateusz Mackiewicz <mateuszmackiewicz97@gmail.com>
2020-11-29 21:50:33 +0100

    walka z grafami

--- django lab/graf.py
+++ django lab/graf.py
@@ -1,3 +1,8 @@
+import random
+import math
+import networkx as nx
+import matplotlib.pyplot as plt
+
 KOLOR = ["czerwony", "niebieski"]
 
 class Graf:
@@ -28,15 +33,18 @@ class Graf:
                     return x.sasiedzi
                 elif algorytm == "DFS":
                     return x.sasiedzi[::-1]
-    def ustaw_kolor(self, v, kolor):
+    def ustaw_kolor(self, v, kolor, printowanie):
         for x in self.wierzcholki:
             if x.numer == v:
                 if x.kolor:
                     if x.kolor != kolor:
-                        print(f'Graf niedwudzielny!  Wierzchołek: {x.numer} ma ustawiony kolor {x.kolor}. Próbujesz zmienić na {kolor}.')
+                        if printowanie is True:
+                            print(f'Graf niedwudzielny!  Wierzchołek: {x.numer} ma ustawiony kolor {x.kolor}. Próbujesz zmienić na {kolor}.')
                         self.czy_dwudzielny = False
+                        return 1
                 else:
-                    print(f'Ustawiam kolor wierzcholka {x.numer} na kolor {kolor}')
+                    if printowanie is True:
+                        print(f'Ustawiam kolor wierzcholka {x.numer} na kolor {kolor}')
                     x.kolor = kolor
 
 class Wierzcholek:
@@ -45,7 +53,7 @@ class Wierzcholek:
         self.sasiedzi = []
         self.kolor = None
     def __repr__(self):
-        return f'Wierzchołek: {self.numer} - sąsiedzi: {"".join(map(str,self.sasiedzi))}'
+        return f'Wierzchołek: {self.numer} - sąsiedzi: {"-".join(map(str,self.sasiedzi))}'
     def zwroc_kolor(self):
         return f'Wierzchołek: {self.numer} - kolor: {self.kolor}'
 
@@ -54,7 +62,7 @@ def algorytm_BFS(graf):
     print("")
     graf.wyswietl_wierzcholki()
     odwiedzeni = []
-    graf.ustaw_kolor(1, KOLOR[1])
+    graf.ustaw_kolor(1, KOLOR[1], True)
     kolejka = [1]
     x = 1
     while kolejka:
@@ -66,20 +74,24 @@ def algorytm_BFS(graf):
             dodawani = [wierzcholek for wierzcholek in dodawani if wierzcholek not in odwiedzeni]
             nrkoloru = KOLOR.index(graf.wierzcholki[aktualny_wierzcholek-1].kolor)
             for wierzcholek in dodawani:
-                graf.ustaw_kolor(wierzcholek, KOLOR[(nrkoloru+1)%2])
+                graf.ustaw_kolor(wierzcholek, KOLOR[(nrkoloru+1)%2], True)
             dodawani = [wierzcholek for wierzcholek in dodawani if wierzcholek not in kolejka]
             kolejka.extend(dodawani)
         except:
             pass
         odwiedzeni.append(aktualny_wierzcholek)
         print(f'Odwiedzone: {"".join(map(str, odwiedzeni))}, na stosie: {"".join(map(str, kolejka))}')
+    print("")
+    print(f'Czy graf dwudzielny: {graf.czy_dwudzielny}')
+    print("")
+    graf.wyswietl_kolor()
 
 def algorytm_DFS(graf):
     print("/"*10+" ALGORYTM DFS "+"\\"*10)
     print("")
     graf.wyswietl_wierzcholki()
     odwiedzeni = []
-    graf.ustaw_kolor(1, KOLOR[1])
+    graf.ustaw_kolor(1, KOLOR[1], True)
     kolejka = [1]
     x = 1
     while kolejka:
@@ -91,106 +103,91 @@ def algorytm_DFS(graf):
             dodawani = [wierzcholek for wierzcholek in dodawani if wierzcholek not in odwiedzeni]
             nrkoloru = KOLOR.index(graf.wierzcholki[aktualny_wierzcholek-1].kolor)
             for wierzcholek in dodawani:
-                graf.ustaw_kolor(wierzcholek, KOLOR[(nrkoloru+1)%2])
+                graf.ustaw_kolor(wierzcholek, KOLOR[(nrkoloru+1)%2], True)
             dodawani = [wierzcholek for wierzcholek in dodawani if wierzcholek not in kolejka]
             kolejka.extend(dodawani)
         except:
             pass
         odwiedzeni.append(aktualny_wierzcholek)
         print(f'Odwiedzone: {"".join(map(str, odwiedzeni))}, na stosie: {"".join(map(str, kolejka))}')
+    print("")
+    print(f'Czy graf dwudzielny: {graf.czy_dwudzielny}')
+    print("")
+    graf.wyswietl_kolor()
+
+def algorytm_DFS_bez_print(graf):
+    odwiedzeni = []
+    graf.ustaw_kolor(1, KOLOR[1], False)
+    kolejka = [1]
+    while kolejka:
+        aktualny_wierzcholek = kolejka.pop()
+        try:
+            dodawani = graf.zwroc_sasiadow(aktualny_wierzcholek, "DFS")
+            dodawani = [wierzcholek for wierzcholek in dodawani if wierzcholek not in odwiedzeni]
+            nrkoloru = KOLOR.index(graf.wierzcholki[aktualny_wierzcholek-1].kolor)
+            for wierzcholek in dodawani:
+                if graf.ustaw_kolor(wierzcholek, KOLOR[(nrkoloru+1)%2], False) == 1:
+                    return False
+            dodawani = [wierzcholek for wierzcholek in dodawani if wierzcholek not in kolejka]
+            kolejka.extend(dodawani)
+        except:
+            pass
+        odwiedzeni.append(aktualny_wierzcholek)
+    return graf.czy_dwudzielny
 
 def wczytaj_dane(plik):
     with open (plik, "r") as p:
+         kraw = []
          dane = p.readlines()
          liczba_wierzcholkow, liczba_krawedzi = dane.pop(0).split()
          graf = Graf(int(liczba_wierzcholkow), int(liczba_krawedzi))
          for line in dane:
             v, w = line.split()
             graf.dodaj(int(v), int(w))
-    return graf
-
-# graf = wczytaj_dane("dane_graf.txt")
-# algorytm_BFS(graf)
-# print("")
-# algorytm_DFS(graf)
-# print("")
-# print(f'Czy graf dwudzielny: {graf.czy_dwudzielny}')
-# print("")
-# graf.wyswietl_kolor()
+            kraw.append((v,w))
+    return graf, kraw
 
 def stworz(liczba_wierzcholkow, gestosc):
     graf = Graf(liczba_wierzcholkow, 0)
     kraw = []
-    for k in range(1, liczba_wierzcholkow):
-        graf.dodaj(k, k+1)
-        graf.dodaj(k+1, k)
-        kraw.append((k, k+1))
-        kraw.append((k+1, k))
-        graf.liczba_krawedzi += 2
-    graf.dodaj(liczba_wierzcholkow, 1)
-    kraw.append((liczba_wierzcholkow, 1))
-    graf.liczba_krawedzi += 1
-    qmin = 5 * round(graf.liczba_krawedzi / (liczba_wierzcholkow * (liczba_wierzcholkow - 1)) * 100 / 5)
+    kombinacje = []
     for i in range(1, liczba_wierzcholkow):
-        for j in range(i+2, liczba_wierzcholkow + 1):
+        for j in range(i+1, liczba_wierzcholkow + 1):
             if j != i:
-                graf.dodaj(i, j)
-                kraw.append((i, j))
-                if i == 1 and j == liczba_wierzcholkow:
-                    pass
-                else:
-                    kraw.append((j, i))
-                    graf.dodaj(j, i)
-                q = 5*round(graf.liczba_krawedzi/(liczba_wierzcholkow*(liczba_wierzcholkow-1))*100/5)
-                if q == gestosc:
-                    return graf, kraw
-                graf.liczba_krawedzi += 2
-    #raise Exception(f'Podano zbyt małą gęstość grafu - {gestosc}! Minimalna wartość wynosi: {qmin}')
-g1, kraw= stworz(10, 100)
-grafy = {}
-for i in range(2):
-    for q in range(20, 105, 5):
-        g,k = stworz(10, q)
-        if q in grafy:
-            grafy[q].append(g)
-        else:
-            grafy[q] = [g]
-print(grafy)
-dwudzielnosc = {}
-for q, grafs in grafy.items():
-    for g in grafs:
-        algorytm_DFS(g)
-        if q in dwudzielnosc:
-            if g.czy_dwudzielny:
-                dwudzielnosc[q][0] += 1
-            else:
-                dwudzielnosc[q][1] += 1
-        else:
-            if g.czy_dwudzielny:
-                dwudzielnosc[q] = [1, 0]
-            else:
-                dwudzielnosc[q] = [0, 1]
-
-print(dwudzielnosc)
-
-
-
-#algorytm_BFS(g1)
-print("")
-#algorytm_DFS(g1)
-print("")
-print(f'Czy graf dwudzielny: {g1.czy_dwudzielny}')
-print("")
-import networkx as nx
-import matplotlib.pyplot as plt
-G = nx.Graph()
-G.add_nodes_from(range(1,10))
-G.add_edges_from(kraw)
-nx.draw(G, with_labels=True)
-plt.show()
-#g1.wyswietl_kolor()
+                kombinacje.append((i, j))
+    do_polaczenia = [x for x in range(1, liczba_wierzcholkow+1)]
+    vw = random.choice(kombinacje)
+    v, w = vw
+    kombinacje.remove(vw)
+    do_polaczenia.remove(w)
+    for i in range(liczba_wierzcholkow-2):
+        graf.dodaj(v, w)
+        graf.dodaj(w, v)
+        kraw.append((v, w))
+        kraw.append((w, v))
+        graf.liczba_krawedzi += 2
+        for k in kombinacje:
+            if v in k and w in k:
+                kombinacje.remove(k)
+                break
+        w = v
+        do_polaczenia.remove(v)
+        v = random.choice(do_polaczenia)
+    liczba_krawedzi = gestosc/100*liczba_wierzcholkow*(liczba_wierzcholkow-1)
+    liczba_krawedzi -= graf.liczba_krawedzi
+    liczba_krawedzi = math.ceil(liczba_krawedzi/2)
+    for i in range(liczba_krawedzi):
+        vw = random.choice(kombinacje)
+        kombinacje.remove(vw)
+        kraw.append((vw[0], vw[1]))
+        kraw.append((vw[1], vw[0]))
+        graf.dodaj(vw[0], vw[1])
+        graf.dodaj(vw[1], vw[0])
+        graf.liczba_krawedzi+=2
+    return graf, kraw
 
 def stworz_graf(liczba_wierzcholkow, gestosc):
+    G = nx.Graph()
     print("Minimalna gęstość grafu wynosi: ", nx.density(G)*100)
     for i in range(liczba_wierzcholkow):
         for j in range(i, liczba_wierzcholkow+1):
@@ -199,13 +196,10 @@ def stworz_graf(liczba_wierzcholkow, gestosc):
                 if (5*round(nx.density(G)*100/5)) == gestosc:
                     print(nx.density(G)*100)
     return G
-    #raise Exception
-
 
 #G = stworz_graf(10, 25)
 #nx.draw(G, with_labels=True)
 #G = nx.tetrahedral_graph()
-
 #G = nx.complete_bipartite_graph(5, 10)
 #G = nx.barbell_graph(5,6)
 #G = nx.lollipop_graph(10, 5)
@@ -214,7 +208,55 @@ def stworz_graf(liczba_wierzcholkow, gestosc):
 #nx.draw_shell(G, nlist=[range(5,10), range(5)], with_labels=True)
 #print(G.get_edge_data())
 #print(G.neighbors(0))
-
 #print(G.nodes)
 #print(G.number_of_edges())
 #print(G.number_of_nodes())
+
+def wyswietl_graf(krawedzie):
+    G = nx.Graph()
+    G.add_edges_from(krawedzie)
+    nx.draw(G, with_labels=True)
+    #print(nx.density(G))
+    plt.show()
+
+def wersjaA(plik):
+    graf, kraw = wczytaj_dane(plik)
+    algorytm_BFS(graf)
+    algorytm_DFS(graf)
+    wyswietl_graf(kraw)
+
+def wersjaB():
+    grafy = {}
+    for i in range(10):
+        for q in range(5, 101, 10):
+            g,k = stworz(10, q)
+            #wyswietl_graf(k)
+            #g.wyswietl_wierzcholki()
+            if q in grafy:
+                grafy[q].append(g)
+            else:
+                grafy[q] = [g]
+    dwudzielnosc = {}
+    for q, grafs in grafy.items():
+        for g in grafs:
+            algorytm_DFS_bez_print(g)
+            if q in dwudzielnosc:
+                if g.czy_dwudzielny:
+                    dwudzielnosc[q][0] += 1
+                else:
+                    dwudzielnosc[q][1] += 1
+            else:
+                if g.czy_dwudzielny:
+                    dwudzielnosc[q] = [1, 0]
+                else:
+                    dwudzielnosc[q] = [0, 1]
+
+    print(dwudzielnosc)
+
+#wersjaA("dane_graf3.txt")
+
+wersjaB()
+#g1, kraw= stworz(10, 30)
+#g1.wyswietl_wierzcholki()
+#algorytm_DFS(g1)
+#print(f'Czy graf dwudzielny: {g1.czy_dwudzielny}')
